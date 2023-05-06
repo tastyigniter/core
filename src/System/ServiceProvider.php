1d735126e9c4e6007471a8c01df6fbe4e8d6ded2
Sam <6567634+sampoyigi@users.noreply.github.com>
2023-05-06 18:05:42 +0100

    Add ConsoleCommandSubscriber class to handle console events

--- src/System/ServiceProvider.php
+++ src/System/ServiceProvider.php
@@ -6,13 +6,12 @@ use Igniter\Flame\Flash\FlashBag;
 use Igniter\Flame\Igniter;
 use Igniter\Flame\Providers\AppServiceProvider;
 use Igniter\Flame\Setting\Facades\Setting;
+use Igniter\System\EventSubscribers\ConsoleCommandSubscriber;
 use Igniter\System\Exception\ErrorHandler;
 use Igniter\System\Template\Extension\BladeExtension;
-use Illuminate\Console\Events\CommandFinished;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Events\MigrationsStarted;
 use Illuminate\Foundation\AliasLoader;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Schema;
@@ -130,15 +129,7 @@ class ServiceProvider extends AppServiceProvider
             $this->registerConsoleCommand($command, $class);
         }
 
-        $commandMap = [
-            'package:discover' => 'igniter:package-discover',
-        ];
-
-        Event::listen(CommandFinished::class, function (CommandFinished $event) use ($commandMap) {
-            if ($igniterCommand = array_get($commandMap, $event->command)) {
-                Artisan::call($igniterCommand, [], $event->output);
-            }
-        });
+        Event::subscribe(ConsoleCommandSubscriber::class);
     }
 
     /*
