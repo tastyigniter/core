4602dedfef44e4a27b0b2cee4f4ff777e15a2d5b
sampoyigi <sampoyigi@users.noreply.github.com>
2023-05-19 20:25:12 +0000

    PHP Linting (Pint) - Fix styling

--- src/Main/Classes/ThemeManager.php
+++ src/Main/Classes/ThemeManager.php
@@ -717,6 +717,6 @@ class ThemeManager
 
         File::makeDirectory($path, 0777, false, true);
 
-        File::put($path.'/theme.json', json_encode($themeConfig, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
+        File::put($path.'/theme.json', json_encode($themeConfig, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
 }
