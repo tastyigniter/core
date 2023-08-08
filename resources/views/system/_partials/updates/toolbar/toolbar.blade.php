8add0ba405f010e16ad92652ef5ec2532e773803
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-08-08 19:57:17 +0100

    Minor fix
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- resources/views/system/_partials/updates/toolbar/toolbar.blade.php
+++ resources/views/system/_partials/updates/toolbar/toolbar.blade.php
@@ -11,7 +11,7 @@
         <div class="progress-indicator-container">
             <div class="d-flex justify-content-between align-items-center">
                 <div>
-                    @if ($updates['items']->isNotEmpty())
+                    @if (isset($updates['items']) && $updates['items']->isNotEmpty())
                         <button
                             type="button"
                             class="btn btn-primary pull-left mr-2 ml-0"
--- resources/views/system/updates/index.blade.php
+++ resources/views/system/updates/index.blade.php
@@ -3,13 +3,13 @@
   {!! $this->widgets['toolbar']->render() !!}
 
   <div class="card shadow-sm mx-3">
-    @if ($updates['items']->isNotEmpty() || $updates['ignoredItems']->isNotEmpty())
+    @if (isset($updates) && ($updates['items']->isNotEmpty() || $updates['ignoredItems']->isNotEmpty()))
       <div id="updates">
         {!! $this->makePartial('updates/list') !!}
       </div>
     @else
-      <div class="panel-body" id="list-items">
-        <h5 class="text-w-400 mb-0">@lang('igniter::system.updates.text_no_updates')</h5>
+          <div class="card-body" id="list-items">
+              <h5 class="text-w-400 mb-0">@lang('igniter::system.updates.text_no_updates')</h5>
       </div>
     @endif
   </div>
