aa04dd912c09313ed87ee36aafa62080d70ae0bc
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-12-12 19:02:21 +0000

    Remove unused queries from seeder
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/System/Database/Seeds/UpdateRecordsSeeder.php
+++ src/System/Database/Seeds/UpdateRecordsSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Igniter\System\Database\Seeds;
 
-use Igniter\Cart\Models\Category;
-use Igniter\Local\Models\Location;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -19,48 +17,7 @@ class UpdateRecordsSeeder extends Seeder
      */
     public function run()
     {
-        $this->updateMorphsOnStatusHistory();
-
-        $this->fixPermalinkSlugColumns();
-
-        $this->fillColumnsOnMailTemplatesData();
-
         $this->updateDiskColumnOnMediaAttachments();
-    }
-
-    protected function updateMorphsOnStatusHistory()
-    {
-        $types = [\Igniter\Cart\Models\Order::class, \Igniter\Reservation\Models\Reservation::class];
-        if (DB::table('status_history')->whereIn('object_type', $types)->count()) {
-            return;
-        }
-
-        $morphs = [
-            'order' => \Igniter\Cart\Models\Order::class,
-            'reserve' => \Igniter\Reservation\Models\Reservation::class,
-        ];
-
-        DB::table('status_history')->get()->each(function ($model) use ($morphs) {
-            $status = DB::table('statuses')->where('status_id', $model->status_id)->first();
-            if (!$status || !isset($morphs[$status->status_for])) {
-                return false;
-            }
-
-            DB::table('status_history')->where('status_history_id', $model->status_history_id)->update([
-                'object_type' => $morphs[$status->status_for],
-            ]);
-        });
-    }
-
-    protected function fixPermalinkSlugColumns()
-    {
-        Category::whereNull('permalink_slug')->get()->each->save();
-        Location::whereNull('permalink_slug')->get()->each->save();
-    }
-
-    protected function fillColumnsOnMailTemplatesData()
-    {
-        DB::table('mail_templates')->update(['is_custom' => 1]);
     }
 
     protected function updateDiskColumnOnMediaAttachments()
