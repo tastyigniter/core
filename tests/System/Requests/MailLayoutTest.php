cfad8fdf6296d74d865c6ff662db55715107669c
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2023-12-31 14:47:23 +0100

    add tests for mail layout and mail partial

--- tests/System/Requests/MailLayoutTest.php
+++ tests/System/Requests/MailLayoutTest.php
@@ -3,8 +3,9 @@
 namespace Tests\System\Requests;
 
 use Igniter\System\Requests\MailLayoutRequest;
+use Illuminate\Validation\Rule;
 
-it('has required rule for inputs', function () {
+it('has required rule for inputs: name and code', function () {
     $rules = (new MailLayoutRequest)->rules();
 
     expect('required')->toBeIn(array_get($rules, 'name'))
@@ -15,6 +16,35 @@ it('has regex rule for code input', function () {
     expect('regex:/^[a-z-_\.\:]+$/i')->toBeIn(array_get((new MailLayoutRequest())->rules(), 'code'));
 });
 
-it('has max characters rule for code input', function () {
+it('has sometimes rule for code input', function () {
+    expect('sometimes')->toBeIn(array_get((new MailLayoutRequest())->rules(), 'code'));
+});
+
+it('has characters length between 2 and 32 characters rule for code input', function () {
     expect('between:2,32')->toBeIn(array_get((new MailLayoutRequest)->rules(), 'name'));
+});
+
+it('has unique rule for code input', function () {
+    expect((string)(Rule::unique('mail_layouts')->ignore(null, 'layout_id')))
+        ->toBeIn(
+            collect(array_get((new MailLayoutRequest)->rules(), 'code'))->map(function ($rule) {
+                return (string)$rule;
+            })->toArray()
+        );
+});
+
+it('has string rule for inputs: layout_css, name, plain_layout and layout', function () {
+    $rules = (new MailLayoutRequest)->rules();
+
+    expect('string')->toBeIn(array_get($rules, 'layout'))
+        ->and('string')->toBeIn(array_get($rules, 'layout_css'))
+        ->and('string')->toBeIn(array_get($rules, 'name'))
+        ->and('string')->toBeIn(array_get($rules, 'plain_layout'));
+});
+
+it('has nullable rule for input rules: layout_css and plain_layout', function () {
+    $rules = (new MailLayoutRequest)->rules();
+
+    expect('nullable')->toBeIn(array_get($rules, 'layout_css'))
+        ->and('nullable')->toBeIn(array_get($rules, 'plain_layout'));
 });
---
+++ tests/System/Requests/MailPartialTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\System\Requests;
+
+use Igniter\System\Requests\MailPartialRequest;
+use Illuminate\Validation\Rule;
+
+it('has required rule for inputs: name, code and html', function () {
+    $rules = (new MailPartialRequest)->rules();
+
+    expect('required')->toBeIn(array_get($rules, 'name'))
+        ->and('required')->toBeIn(array_get($rules, 'code'))
+        ->and('required')->toBeIn(array_get($rules, 'html'));
+});
+
+it('has regex rule for code input', function () {
+    expect('regex:/^[a-z-_\.\:]+$/i')->toBeIn(array_get((new MailPartialRequest())->rules(), 'code'));
+});
+
+it('has sometimes rule for code input', function () {
+    expect('sometimes')->toBeIn(array_get((new MailPartialRequest())->rules(), 'code'));
+});
+
+it('has unique rule for code input', function () {
+    expect((string)(Rule::unique('mail_partials')->ignore(null, 'partial_id')))
+        ->toBeIn(
+            collect(array_get((new MailPartialRequest)->rules(), 'code'))->map(function ($rule) {
+                return (string)$rule;
+            })->toArray()
+        );
+});
+
+it('has string rule for input rules: html, name, and text', function () {
+    $rules = (new MailPartialRequest)->rules();
+
+    expect('string')->toBeIn(array_get($rules, 'text'))
+        ->and('string')->toBeIn(array_get($rules, 'name'))
+        ->and('string')->toBeIn(array_get($rules, 'html'));
+});
+
+it('has nullable rule for text input', function () {
+    $rules = (new MailPartialRequest)->rules();
+
+    expect('nullable')->toBeIn(array_get($rules, 'text'));
+});
