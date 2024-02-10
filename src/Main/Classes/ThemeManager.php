e9d1018945d871c04aae4967f98e0bbf368c2ad0
sampoyigi <sampoyigi@users.noreply.github.com>
2024-02-10 22:28:23 +0000

    PHP Linting (Pint) - Fix styling

--- src/Main/Classes/ThemeManager.php
+++ src/Main/Classes/ThemeManager.php
@@ -154,7 +154,7 @@ class ThemeManager
 
         collect([$theme->getPath().'/resources', $theme->getPath().'/assets', $theme->getPath()])
             ->merge($theme->hasParent() ? [$theme->getParent()->getPath().'/resources', $theme->getParent()->getPath().'/assets', $theme->getParent()->getPath()] : [])
-            ->filter(fn($path) => File::isDirectory($path))
+            ->filter(fn ($path) => File::isDirectory($path))
             ->reverse()
             ->each(function ($path) use ($theme) {
                 Igniter::loadResourcesFrom($path, $theme->getName());
