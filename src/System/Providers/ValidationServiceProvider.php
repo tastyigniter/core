6863e508f508aca288fb8f02dac0d3181ab4274d
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-03-07 23:38:15 +0000

    Cleanup redundant custom validation rule
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/System/Providers/ValidationServiceProvider.php
+++ src/System/Providers/ValidationServiceProvider.php
@@ -13,23 +13,6 @@ class ValidationServiceProvider extends ServiceProvider
         $this->registerValidator();
     }
 
-    public function boot()
-    {
-        Validator::extend('trim', function ($attribute, $value, $parameters, $validator) {
-            return trim($value);
-        });
-
-        Validator::extend('valid_date', function ($attribute, $value, $parameters, $validator) {
-            return !(!preg_match('/^(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-[0-9]{4}$/', $value)
-                && !preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', $value));
-        });
-
-        Validator::extend('valid_time', function ($attribute, $value, $parameters, $validator) {
-            return !(!preg_match('/^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/', $value)
-                && !preg_match('/^(1[012]|[1-9]):[0-5][0-9](\s)?(?i)(am|pm)$/', $value));
-        });
-    }
-
     protected function registerValidator()
     {
         $this->app->resolving('validator', function ($validator) {
