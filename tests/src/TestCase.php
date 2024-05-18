deaa98c8992b460d454e523b73ea669f2d2b9527
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-05-18 11:40:44 +0100

    Register manually livewire service provider in tests
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- tests/src/TestCase.php
+++ tests/src/TestCase.php
@@ -7,6 +7,7 @@ use Igniter\Main\Classes\ThemeManager;
 use Igniter\System\Classes\PackageManifest;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\View;
+use Livewire\LivewireServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -15,6 +16,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return [
+            LivewireServiceProvider::class,
             \Igniter\Flame\ServiceProvider::class,
         ];
     }
@@ -42,14 +44,14 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     {
         parent::resolveApplicationConfiguration($app);
 
-        $app->afterResolving(PackageManifest::class, function ($instance) {
+        $app->afterResolving(PackageManifest::class, function($instance) {
             $instance->vendorPath = __DIR__.'/../../vendor';
         });
 
         $configs = ['currency', 'geocoder', 'system'];
 
         foreach ($configs as $config) {
-            $app['config']->set("igniter.$config", require (__DIR__."/../../config/{$config}.php"));
+            $app['config']->set("igniter.$config", require(__DIR__."/../../config/{$config}.php"));
         }
     }
 }
