18d515ef9de321fc0e790c69a95ab6bb666a8748
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-08-10 11:15:12 +0100

    Minor fix
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- resources/views/system/_partials/extensions/extension_readme.blade.php
+++ resources/views/system/_partials/extensions/extension_readme.blade.php
@@ -1,7 +1,7 @@
 @if($record && $record->class && strlen($record->readme))
     <div class="modal-dialog modal-dialog-scrollable modal-lg">
     <div class="modal-content">
-        <div class="modal-header">
+        <div class="modal-header justify-content-between">
             <h4 class="modal-title">{{ $record->title }}</h4>
             @isset($record->meta['homepage'])
                 <a href="{{ $record->meta['homepage']}}"><i class="fa fa-external-link fa-2x"></i></a>
