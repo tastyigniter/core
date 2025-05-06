4e102b7efbe52bc52075ab49be80911d867b24ec
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-05-06 22:24:37 +0100

    refactor(lang): moved login translations to user extension

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -349,29 +349,6 @@ return [
         'alert_reset_layout_success' => 'Layout reset successfully',
     ],
 
-    'login' => [
-        'text_title' => 'Login',
-        'text_password_reset_title' => 'Reset Password',
-        'text_forgot_password' => 'Forgot your password?',
-        'text_reset_password_title' => 'Reset your Password',
-        'text_back_to_login' => 'Back to Login',
-
-        'button_login' => 'Login',
-        'button_reset_password' => 'Reset Password',
-
-        'label_reset_code' => 'Reset code',
-        'label_email' => 'Email address',
-        'label_password' => 'Password',
-        'label_password_confirm' => 'Confirm Password',
-
-        'alert_login_failed' => 'Login failed, please try again or contact the system administrator!',
-        'alert_success_reset' => 'You\'ve successfully changed your password.',
-        'alert_failed_reset' => 'The reset code is either invalid or expired.',
-        'alert_success_logout' => 'You are now logged out.',
-        'alert_email_sent' => 'We\'ve sent a password reset link to your email.',
-        'alert_email_not_sent' => 'The e-mail could not be sent. Possible reason: your host may have disabled the mail() function.',
-    ],
-
     'permissions' => [
         'name' => 'Administration',
         'dashboard' => 'Manage dashboard widgets',
