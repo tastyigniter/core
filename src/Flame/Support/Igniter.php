25c72d15d6b1f94db48453e64efb04ee6abcc86b
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-05-28 21:34:18 +0100

    Update version to v4.0.10

--- src/Flame/Support/Igniter.php
+++ src/Flame/Support/Igniter.php
@@ -13,7 +13,7 @@ use Illuminate\View\Factory;
 
 class Igniter
 {
-    protected const string VERSION = 'v4.0.9';
+    protected const string VERSION = 'v4.0.10';
 
     /**
      * The base path for extensions.
