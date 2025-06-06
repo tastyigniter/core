964fe227813ddbf11cd2d442df576c34ecbed590
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-06-06 19:30:53 +0100

    fix: replace save() with saveQuietly() in LogsStatusHistory model
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Admin/Models/Concerns/LogsStatusHistory.php
+++ src/Admin/Models/Concerns/LogsStatusHistory.php
@@ -54,7 +54,7 @@ trait LogsStatusHistory
             return false;
         }
 
-        $this->save();
+        $this->saveQuietly();
 
         $this->fireSystemEvent('admin.statusHistory.added', [$history]);
 
