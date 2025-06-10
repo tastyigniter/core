0c1015b5a2937654000ec1e730c87006851b6b59
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2025-06-10 18:44:14 +0100

    fix "The path file/folder does not exist" issue when uploading to media manager

--- src/System/Console/Commands/IgniterInstall.php
+++ src/System/Console/Commands/IgniterInstall.php
@@ -17,6 +17,7 @@ use Illuminate\Console\ConfirmableTrait;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Console\Input\InputOption;
 
 /**
@@ -234,12 +235,12 @@ class IgniterInstall extends Command
 
     protected function createMediaDirectories(): void
     {
-        $mediaPath = storage_path('app/media');
+        $mediaPath = storage_path('app/public/media');
         if (!File::exists($mediaPath)) {
             File::makeDirectory($mediaPath, 0755, true);
         }
 
-        $mediaPath = storage_path('app/media/uploads');
+        $mediaPath = storage_path('app/public/media/uploads');
         if (!File::exists($mediaPath)) {
             File::makeDirectory($mediaPath, 0755, true);
         }
