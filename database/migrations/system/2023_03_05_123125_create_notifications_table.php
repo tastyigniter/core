bd35a2bfee1355661b71d4009aaa468e9c2cdee4
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2025-06-18 15:00:03 +0100

    rename the index of columns and keys to fix migration name too long issue (#27)

--- database/migrations/system/2023_03_05_123125_create_notifications_table.php
+++ database/migrations/system/2023_03_05_123125_create_notifications_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
         Schema::create('notifications', function(Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('type');
-            $table->morphs('notifiable');
+            $table->morphs('notifiable', 'notifications_notifiable');
             $table->text('data');
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
