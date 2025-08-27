ec4a2981c9766110984c517872c9980929c780b0
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-08-27 10:04:02 +0100

    feat(tests): update filesystem tests to use unique storage paths instead of temp paths

--- tests/src/Flame/Filesystem/FilesystemTest.php
+++ tests/src/Flame/Filesystem/FilesystemTest.php
@@ -10,11 +10,11 @@ use Illuminate\Filesystem\FilesystemAdapter;
 use League\Flysystem\Local\LocalFilesystemAdapter;
 use ReflectionClass;
 
-beforeEach(function() {
+beforeEach(function () {
     $this->filesystem = new Filesystem;
 });
 
-it('symbolizes path', function() {
+it('symbolizes path', function () {
     Igniter::loadResourcesFrom(__DIR__.'/../../../resources/themes/tests-theme', 'tests.fixtures');
 
     $path = resolve(Filesystem::class)->symbolizePath('tests.fixtures::theme.json');
@@ -22,10 +22,10 @@ it('symbolizes path', function() {
     expect($path)->toEndWith('resources/themes/tests-theme/theme.json');
 });
 
-it('checks directory is empty', function() {
-    expect($this->filesystem->isDirectoryEmpty(temp_path().'/unreadable_directory'))->toBeTrue();
+it('checks directory is empty', function () {
+    expect($this->filesystem->isDirectoryEmpty(storage_path('/').uniqid('temp').'-unreadable_directory'))->toBeTrue();
 
-    $directory = temp_path().'/empty_directory';
+    $directory = storage_path('/').uniqid('temp').'-empty_directory';
     mkdir($directory);
     expect($this->filesystem->isDirectoryEmpty($directory))->toBeTrue();
     rmdir($directory);
@@ -33,7 +33,7 @@ it('checks directory is empty', function() {
     expect($this->filesystem->isDirectoryEmpty(__DIR__.'/fixtures/test_directory'))->toBeFalse();
 });
 
-it('converts bytes to human readable format', function() {
+it('converts bytes to human readable format', function () {
     expect($this->filesystem->sizeToString(1073741824))->toBe('1.00 GB')
         ->and($this->filesystem->sizeToString(1048576))->toBe('1.00 MB')
         ->and($this->filesystem->sizeToString(1024))->toBe('1.00 KB')
@@ -42,31 +42,31 @@ it('converts bytes to human readable format', function() {
         ->and($this->filesystem->sizeToString(0))->toBe('0 bytes');
 });
 
-it('returns public path from absolute path', function() {
+it('returns public path from absolute path', function () {
     $publicPath = public_path('welcome');
     expect($this->filesystem->localToPublic($publicPath))->toBe('/welcome');
 });
 
-it('checks path is within application path', function() {
+it('checks path is within application path', function () {
     $file = base_path('composer.json');
     expect($this->filesystem->isLocalPath($file))->toBeTrue()
         ->and($this->filesystem->isLocalPath('/some/external/directory'))->toBeFalse();
 });
 
-it('checks if disk is using local driver', function() {
+it('checks if disk is using local driver', function () {
     $adapter = mock(LocalFilesystemAdapter::class);
     $disk = mock(FilesystemAdapter::class);
     $disk->shouldReceive('getAdapter')->andReturn($adapter);
     expect($this->filesystem->isLocalDisk($disk))->toBeTrue();
 });
 
-it('returns file path from class name', function() {
+it('returns file path from class name', function () {
     $className = Filesystem::class;
     $reflector = new ReflectionClass($className);
     expect($this->filesystem->fromClass($className))->toBe($reflector->getFileName());
 });
 
-it('checks file exist case insensitively', function() {
+it('checks file exist case insensitively', function () {
     $filesystem = mock(Filesystem::class)->makePartial();
     $filesystem->shouldReceive('exists')->andReturn(false, false, false, true);
     $filesystem->shouldReceive('glob')->andReturn(false, ['test_directory/file.txt']);
@@ -76,12 +76,12 @@ it('checks file exist case insensitively', function() {
         ->and($filesystem->existsInsensitive('test_directory/file.txt'))->toBe('test_directory/file.txt');
 });
 
-it('returns normalized path', function() {
+it('returns normalized path', function () {
     $path = 'some\\windows\\path';
     expect($this->filesystem->normalizePath($path))->toBe('some/windows/path');
 });
 
-it('returns symbolized path', function() {
+it('returns symbolized path', function () {
     $this->filesystem->addPathSymbol('@', base_path('modules'));
     $path = '@::some_module';
     expect($this->filesystem->symbolizePath($path, false, false))->toBe(base_path('modules/some_module'))
@@ -91,8 +91,8 @@ it('returns symbolized path', function() {
         ->and($this->filesystem->pathSymbols['@'])->toContain(base_path('modules'));
 });
 
-it('writes contents to a file and sets permissions', function() {
-    $path = temp_path().'/'.fake()->lexify('???.txt');
+it('writes contents to a file and sets permissions', function () {
+    $path = storage_path('/').uniqid('temp').'-'.fake()->lexify('???.txt');
     @unlink($path);
     $contents = 'test contents';
     $this->filesystem->put($path, $contents);
@@ -100,9 +100,9 @@ it('writes contents to a file and sets permissions', function() {
     unlink($path);
 });
 
-it('copies a file to a new location and sets permissions', function() {
-    $source = temp_path().'/'.fake()->lexify('???.txt');
-    $target = temp_path().'/'.fake()->lexify('???.txt');
+it('copies a file to a new location and sets permissions', function () {
+    $source = storage_path('/').uniqid('temp').'-'.fake()->lexify('???.txt');
+    $target = storage_path('/').uniqid('temp').'-'.fake()->lexify('???.txt');
     file_put_contents($source, 'test contents');
     expect($this->filesystem->copy($source, $target))->toBeTrue()
         ->and(file_get_contents($target))->toBe('test contents');
@@ -110,8 +110,8 @@ it('copies a file to a new location and sets permissions', function() {
     unlink($target);
 });
 
-it('creates a directory and sets permissions', function() {
-    $path = temp_path().'/'.fake()->lexify('???');
+it('creates a directory and sets permissions', function () {
+    $path = storage_path('/').uniqid('temp').'-'.fake()->lexify('???');
     $subPath = $path.'/'.fake()->lexify('???');
     $filesystem = mock(Filesystem::class)->makePartial();
     $filesystem->folderPermissions = '0755';
@@ -126,8 +126,8 @@ it('creates a directory and sets permissions', function() {
     rmdir($path);
 });
 
-it('modifies file permissions', function() {
-    $path = temp_path().'/'.fake()->lexify('???.txt');
+it('modifies file permissions', function () {
+    $path = storage_path('/').uniqid('temp').'-'.fake()->lexify('???.txt');
     $filesystem = mock(Filesystem::class)->makePartial();
     $filesystem->shouldReceive('isDirectory')->times(3)->andReturn(true, false);
     expect($filesystem->chmod($path))->toBeFalse(); // file permission is not set
@@ -136,12 +136,13 @@ it('modifies file permissions', function() {
     $filesystem->folderPermissions = '0777';
     expect($filesystem->chmod($path))->toBeBool(); // file permission is set
 
-    $filesystem->shouldReceive('dirname')->once()->andReturn(temp_path().'/test_directory');
-    $filesystem->makeDirectory(temp_path().'/test_directory', 0755, true, true);
-    rmdir(temp_path().'/test_directory');
+    $directory = storage_path('/').uniqid('temp').'-test_directory';
+    $filesystem->shouldReceive('dirname')->once()->andReturn($directory);
+    $filesystem->makeDirectory($directory, 0755, true, true);
+    rmdir($directory);
 });
 
-it('modifies directory permissions recursively', function() {
+it('modifies directory permissions recursively', function () {
     $path = __DIR__.'/fixtures';
     $filesystem = mock(Filesystem::class)->makePartial();
     expect($filesystem->chmodRecursive($path))->toBeFalse();
@@ -153,17 +154,17 @@ it('modifies directory permissions recursively', function() {
         ->and($filesystem->chmodRecursive($path))->toBeNull();
 });
 
-it('returns default file permission mask', function() {
+it('returns default file permission mask', function () {
     $this->filesystem->filePermissions = '0644';
     expect($this->filesystem->getFilePermissions())->toBe(0644);
 });
 
-it('returns default folder permission mask', function() {
+it('returns default folder permission mask', function () {
     $this->filesystem->folderPermissions = '0755';
     expect($this->filesystem->getFolderPermissions())->toBe(0755);
 });
 
-it('matches filename against pattern', function() {
+it('matches filename against pattern', function () {
     $fileName = 'test_file.txt';
     $pattern = '*.txt';
     expect($this->filesystem->fileNameMatch($fileName, $fileName))->toBeTrue()
