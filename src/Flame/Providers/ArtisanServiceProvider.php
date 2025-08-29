c287eecc0f9038e1b2c3c96176782ee77591f956
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-08-29 23:09:34 +0100

    fix: bring back console.schedule to run registered schedule command

--- src/Flame/Providers/ArtisanServiceProvider.php
+++ src/Flame/Providers/ArtisanServiceProvider.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace Igniter\Flame\Providers;
 
+use Illuminate\Console\Application;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Providers\ArtisanServiceProvider as BaseServiceProvider;
 
-class ArtisanServiceProvider extends BaseServiceProvider {}
+class ArtisanServiceProvider extends BaseServiceProvider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            Application::starting(function() {
+                $this->app['events']->dispatch('console.schedule', [$this->app->make(Schedule::class)]);
+            });
+        }
+    }
+}
