16464169781bb3b01c1d19ad4e4a90d12b124d47
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-11-12 23:33:16 +0000

    test: update ModelActionTest to remove redundant parentheses in expect statement

--- tests/src/System/Actions/ModelActionTest.php
+++ tests/src/System/Actions/ModelActionTest.php
@@ -28,5 +28,5 @@ it('throws exception if required property is missing', function() {
 });
 
 it('initializes with null model', function() {
-    expect(new ModelAction())->not->toBeNull();
+    expect(new ModelAction)->not->toBeNull();
 });
