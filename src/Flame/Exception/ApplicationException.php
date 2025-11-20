244d547ad2fafda37d97da0773c23323fec34c8a
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2025-11-20 11:28:09 +0100

    refactor: update constructor type hints in ApplicationException class and remove render function

--- src/Flame/Exception/ApplicationException.php
+++ src/Flame/Exception/ApplicationException.php
@@ -15,25 +15,8 @@ class ApplicationException extends Exception
      * @param int $code Error code.
      * @param Exception|null $previous Previous exception.
      */
-    public function __construct($message = '', $code = 500, ?Exception $previous = null)
+    public function __construct(string $message = '', int $code = 500, ?Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
-    }
-
-    public function render(Request $request): Response
-    {
-        $message = $this->getMessage();
-
-        if (config('app.debug', false)) {
-            $message = sprintf('"%s" on line %s of %s',
-                $this->getMessage(),
-                $this->getLine(),
-                $this->getFile()
-            );
-
-            $message .= $this->getTraceAsString();
-        }
-
-        return response($message, $this->code);
     }
 }
