cc3c2eb1ddc0be44755c1fad124a5984d8eebd77
sampoyigi <6567634+sampoyigi@users.noreply.github.com>
2026-06-14 00:16:37 +0100

    test(main): skip `CheckInitialSetup` middleware in page route binding test

--- tests/src/Main/Providers/PagicServiceProviderTest.php
+++ tests/src/Main/Providers/PagicServiceProviderTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Igniter\Tests\Main\Providers;
 
+use Igniter\Main\Http\Middleware\CheckInitialSetup;
+
 it('resolves page route binding', function() {
-    $result = $this->get('/components');
+    $result = $this->withoutMiddleware(CheckInitialSetup::class)->get('/components');
     $result->assertStatus(200);
     $result->assertSee('This is a test component partial content');
 });
