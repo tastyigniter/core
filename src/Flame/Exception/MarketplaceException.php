687079524837e6d02da58c3f9f21761d58af6e42
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2026-06-18 17:32:37 +0100

    Release v4.3.0 (#59)
    * refactor(exception): use imported `Throwable` instead of FQCN in `MarketplaceException`
    
    * refactor(system): remove `MarketplaceErrorPresenter` class and its tests

--- src/Flame/Exception/MarketplaceException.php
+++ src/Flame/Exception/MarketplaceException.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Igniter\Flame\Exception;
 
+use Throwable;
+
 class MarketplaceException extends SystemException
 {
     public function __construct(
         string $message,
         public readonly ?string $errorCode = null,
         int $code = 0,
-        ?\Throwable $previous = null,
+        ?Throwable $previous = null,
     ) {
         parent::__construct($message, $code, $previous);
     }
