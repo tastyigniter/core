5dca88ea9a347f71297bbb585bcf63c4d51e0d14
sampoyigi <6567634+sampoyigi@users.noreply.github.com>
2026-08-09 10:12:00 +0100

    feat(support): unlink cached addons path after composer autoload dump
    - Add `Igniter\Flame\Support\ComposerScripts` with a `postAutoloadDump` composer script hook
    - Boot a minimal `Illuminate\Foundation\Application` and delete the cached addons file so it regenerates on next run

---
+++ src/Flame/Support/ComposerScripts.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Igniter\Flame\Support;
+
+use Composer\Script\Event;
+use Illuminate\Foundation\Application;
+
+class ComposerScripts
+{
+    public static function postAutoloadDump(Event $event)
+    {
+        require_once $event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php';
+
+        $laravel = new Application(getcwd());
+
+        if (is_file($path = $laravel->make(Igniter::class)->getCachedAddonsPath())) {
+            unlink($path);
+        }
+    }
+}
